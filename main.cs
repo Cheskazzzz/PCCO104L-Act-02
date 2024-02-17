3b400b16a191e01b57d00773225386f88b82494c
Cheskazzzz <159403221+Cheskazzzz@users.noreply.github.com>
2024-02-17 02:13:31 +0000

    Done

--- main.cs
+++ main.cs
@@ -1,7 +1,30 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public static void Main(string [] args)
+    {
+        int X = 30;
+      
+        for (int i = 1; i <= X; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+               Console.WriteLine($"{i} - FooBar");
+            }
+            else if (i % 3 == 0)
+            {
+Console.WriteLine($"{i} - Foo");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine($"{i} - Bar");
+
+            }
+            else
+            {
+              Console.WriteLine(i);
+            }
+        }
+    }
 }
+
